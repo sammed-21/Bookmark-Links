1a721943932a3fdfb751cecf2a6719db052dd8f3
sammed-21 <sammed2110@gmail.com>
2023-08-04 10:43:46 +0530

    connect prisma-client to postgres

---
+++ graphql/schema.ts
@@ -0,0 +1,18 @@
+ 
+export const typeDefs = `
+  type Link {
+    id: ID
+    title: String
+    description: String
+    url: String
+    category: String
+    imageUrl: String
+    users: [String]
+  }
+
+  type Query {
+    links: [Link]!
+    link(id: ID!): Link!
+  }
+  
+`
